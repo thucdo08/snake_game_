c1b3ae1e182132260d22fc0d237f5073a2d263e0
lethuongde290605 <153986981+lethuongde290605@users.noreply.github.com>
2024-03-19 22:10:20 +0700

    Update SetUp.h

--- officialSnake/SetUp.h
+++ officialSnake/SetUp.h
@@ -1,7 +1,19 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
+
+enum eDirection { STOP, DOWN, UP, RIGHT, LEFT, ENTER, EXIT };
+enum Page { MENU, PLAY, HELP, LOADGAME};
+enum cursorMenu { NEWGAME, curHELP, SETTING, RANKING, EXITGAME };
+
+extern eDirection dir;
+extern Page page;
+extern cursorMenu cursor;
+
+extern bool isStart;
+
 
 void studentID(short length);
