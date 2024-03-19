24e0c7ee92adbd4226199d1a617700b4f737e5f0
lethuongde290605 <153986981+lethuongde290605@users.noreply.github.com>
2024-03-19 22:08:50 +0700

    Update Level.h

--- officialSnake/Level.h
+++ officialSnake/Level.h
@@ -4,13 +4,17 @@
 #include "Menu.h"
 #include <Windows.h>
 #include <ctime>
+#include <thread>
 
 using namespace std;
 
-void SetUp();
-void Draw();
+
+void SetUpLvl();
+void Draw(int, int);
 void Input();
 void Logic();
 void drawSnFr();
-void drawBox(int x, int y, int width, int height);
+void gateSnake();
+void drawBox(int x, int y, int width, int height,int, int);
 void level1();
+void level2();
