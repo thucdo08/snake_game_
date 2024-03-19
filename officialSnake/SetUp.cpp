d85d3bd5f6e11a9dd10a15cfce0ccca4b6e629da
lethuongde290605 <153986981+lethuongde290605@users.noreply.github.com>
2024-03-19 22:10:00 +0700

    Update SetUp.cpp

--- officialSnake/SetUp.cpp
+++ officialSnake/SetUp.cpp
@@ -1,7 +1,51 @@
 #include "SetUp.h"
+#include "Header.h"
 
+eDirection dir;
+bool isStart = false;
+Page page;
+cursorMenu cursor;
 
-void studentID(short length) {
-	string id = "23120232";
-	cout << id[length%8];
+void studentID(short length)
+{
+    string id = "23120232";
+    cout << id[length % 8];
+}
+
+void Input()
+{
+        //cout<<"Press W, A, S, D to move, J to enter, E to exit";
+    if (_kbhit())
+    {
+        isStart = true;
+        char temp = tolower(_getch());
+        switch (temp)
+        {
+        case 'w':
+        {
+            if (dir != DOWN || page == MENU)
+                dir = UP;
+            break;
+        }
+        case 'a':
+            if (dir != RIGHT)
+                dir = LEFT;
+            break;
+        case 's': {
+            if (dir != UP || page == MENU)
+                dir = DOWN;
+            break;
+        }
+        case 'd':
+            if (dir != LEFT)
+                dir = RIGHT;
+            break;
+        case 'j':
+            dir = ENTER;
+            break;
+        case 'e':
+            dir = EXIT;
+            break;
+        }
+    }
 }
