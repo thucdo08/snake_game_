3485b5d7577835cd35e68717b505a4da4012b483
thucdo08 <149310854+thucdo08@users.noreply.github.com>
2024-06-07 17:12:52 +0700

    Delete officialSnake (3) directory

--- officialSnake (3)/Sound.cpp
+++
@@ -1,6 +0,0 @@
-#include "Sound.h"
-
-
-void playEatingSound() {
-	PlaySound(TEXT("eatingSound.wav"), NULL, SND_FILENAME | SND_SYNC);
-}
--- officialSnake (3)/Sound.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <Windows.h>
-#include <mmsystem.h>
-#include <thread>
-#pragma comment(lib, "winmm.lib")
-
-void playBackgroundMenuSound();
-void playEatingSound();
